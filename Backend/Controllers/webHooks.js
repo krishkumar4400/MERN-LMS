bfa3eab381faa4861594b3ef3d20d25e43b65f1a
krish kuamr <krishkumarbgp2022@gmail.com>
2025-05-06 19:31:36 +0530

    imgUrl changed

--- Backend/Controllers/webHooks.js
+++ Backend/Controllers/webHooks.js
@@ -19,7 +19,7 @@ export const clerkWebHooks = async (req,res) => {
           _id: data.id,
           email: data.email_addresses[0].email_address,
           name: data.first_name + " " + data.last_name,
-          imageUrl: data.imageUrl,
+          imageUrl: data.image_url,
         }
         await User.create(userData);
         res.json({});
@@ -30,7 +30,7 @@ export const clerkWebHooks = async (req,res) => {
         const userData = {
           email: data.email_addresses[0].email_address,
           name: data.first_name + " " + data.last_name,
-          imageUrl: data.imageUrl
+          imageUrl: data.image_url
         }
 
         await User.findByIdAndUpdate(data.id, userData);
