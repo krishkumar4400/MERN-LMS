62d1a7f117656f2881cf94533f80cffc55b13042
krish kuamr <krishkumarbgp2022@gmail.com>
2025-05-06 19:15:36 +0530

    DB name changed

--- Backend/Config/ConnectDB.js
+++ Backend/Config/ConnectDB.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   mongoose.connection.on('connected', ()=> console.log("Database connected"));
   try {
-    await mongoose.connect(`${process.env.MONGO_URI}/MERN_AUTH`);
+    await mongoose.connect(`${process.env.MONGO_URI}/MERN_LMS`);
     console.log("Database connected Successfully");
   } catch (error) {
     console.log(error.message)
