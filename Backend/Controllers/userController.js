4b2ec769ee51af7cb8646c9adcf401dcf16d577d
krish kuamr <krishkumarbgp2022@gmail.com>
2025-05-11 22:16:36 +0530

    Backend Complete

--- Backend/Controllers/userController.js
+++ Backend/Controllers/userController.js
@@ -2,6 +2,7 @@ import Stripe from "stripe";
 import { Course } from "../Model/Course.js";
 import { User } from "../Model/User.js";
 import { Purchase } from "../Model/Purchase.js";
+import { CourseProgress } from "../Model/CourseProgress.js";
 
 // Get User Data
 export const getUserData = async (req,res) => {
@@ -111,6 +112,119 @@ export const purchaseCourse = async (req,res) => {
       session_url: session.url,
       success: true
     });
+
+  } catch (error) {
+    res.json({
+      message: error.message,
+      success: false
+    });
+  }
+}
+
+// Update User Course Progress
+export const updateUserCourseProgress = async (req,res) => {
+  try {
+    const userId = req.auth.userId;
+    const {courseId, lectureId} = req.body;
+    const progressData = await CourseProgress.findOne({userId, courseId});
+
+    if(progressData) {
+      if(progressData.lectureCompleted.includes(lectureId)) {
+        return res.json({
+          success: true,
+          message: 'Lecture Already Completed'
+        });
+      }
+
+      progressData.lectureCompleted.push(lectureId);
+      await progressData.save();
+    }
+    else {
+      await CourseProgress.create({
+        userId,
+        courseId,
+        lectureCompleted: [lectureId]
+      });
+    }
+
+    res.json({
+      success: true,
+      message: 'Progress Updated'
+    });
+
+  } catch (error) {
+    res.json({
+      message: error.message,
+      success: false
+    });
+  }
+}
+
+// Get User Course Progress
+export const getUserCourseProgress = async (req,res) => {
+  try {
+    const userId = req.auth.userId;
+    const {courseId} = req.body;
+    const progressData = await CourseProgress.findOne({userId, courseId});
+    res.json({
+      success: true,
+      progressData
+    });
+  } catch (error) {
+    res.json({
+      message: error.message,
+      success: false
+    });
+  }
+}
+
+// Add User Rating To Course
+export const addUserRating = async (req,res) => {
+  const userId = req.auth.userId;
+  const {courseId, rating} = req.body;
+
+  if(!userId || !courseId || !rating || !rating || rating < 1 || rating  > 5){
+    return res.json({
+      message: "Invalid details",
+      success: false 
+    });
+  }
+
+  try {
+    const course = await Course.findById(courseId);
+    if(!course) {
+      return res.json({
+        message: 'Course Not Found',
+        success: false
+      });
+    }
+
+    const user = await User.findById(userId);
+    if(!user || !user.enrolledCourses.includes(courseId)) {
+      return res.json({
+        message: "User has not purchased this course",
+        success: false 
+      });
+    }
+
+    const existingRatingIndex = course.courseRatings.findIndex(r => r.userId === userId);
+
+    if(existingRatingIndex > -1) {
+      course.courseRatings[existingRatingIndex].rating = rating;
+    }
+    else{
+      course.courseRatings.push({
+        userId,
+        rating
+      });
+    }
+
+    await Course.save();
+
+    return res.json({
+      success: true,
+      message: 'Rating Added'
+    })
 
   } catch (error) {
     res.json({
--- Backend/Routes/userRoutes.js
+++ Backend/Routes/userRoutes.js
@@ -1,15 +1,31 @@
-import {Router} from 'express';
-import { getUserData, purchaseCourse, userEnrolledCourses } from '../Controllers/userController.js';
+import { Router } from "express";
+import {
+  addUserRating,
+  getUserCourseProgress,
+  getUserData,
+  purchaseCourse,
+  updateUserCourseProgress,
+  userEnrolledCourses,
+} from "../Controllers/userController.js";
 
 const userRouter = Router();
 
 // Get user Data
-userRouter.get('/data', getUserData);
+userRouter.get("/data", getUserData);
 
 // Get user enrolled courses
-userRouter.get('/enrolled-courses', userEnrolledCourses);
+userRouter.get("/enrolled-courses", userEnrolledCourses);
 
 // purchase course (stripe payment)
-userRouter.post('/purchase', purchaseCourse)
+userRouter.post("/purchase", purchaseCourse);
+
+// update course progress
+userRouter.post("/update-course-progress", updateUserCourseProgress);
+
+// get course progress
+userRouter.post("/get-course-progress", getUserCourseProgress);
+
+// Add Rating To The Course
+userRouter.post("/add-rating", addUserRating);
 
 export default userRouter;
